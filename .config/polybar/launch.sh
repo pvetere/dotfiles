7cd17b90b511a08ac0e86a8ebd59461539016bff
Peter Vetere <pvetere@singlestore.com>
2021-08-29 20:23:19 -0400

    more stuff again

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,14 @@ pkill polybar
 
 sleep 1
 
-polybar -q main -c /home/pvetere/.config/polybar/config.ini &
+#polybar -q main -c /home/pvetere/.config/polybar/config.ini &
+polybar -q main_l -c /home/pvetere/.config/polybar/config.ini &
+polybar -q main_c -c /home/pvetere/.config/polybar/config.ini &
+polybar -q main_r -c /home/pvetere/.config/polybar/config.ini &
+bspc config -m eDP-1 top_padding 0
+bspc config -m eDP-1 bottom_padding 30
+
 polybar -q aux -c /home/pvetere/.config/polybar/config.ini &
+bspc config -m HDMI-1 top_padding 25
+bspc config -m HDMI-1 bottom_padding 0
 
