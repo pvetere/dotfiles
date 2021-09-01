c6487b680752e8e6d90edfa93e10d6d29e1a387b
Peter Vetere <pvetere@singlestore.com>
2021-09-01 09:44:21 -0400

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,10 +8,19 @@ sleep 1
 polybar -q main_l -c /home/pvetere/.config/polybar/config.ini &
 polybar -q main_c -c /home/pvetere/.config/polybar/config.ini &
 polybar -q main_r -c /home/pvetere/.config/polybar/config.ini &
+
 bspc config -m eDP-1 top_padding 0
 bspc config -m eDP-1 bottom_padding 30
 
-polybar -q aux -c /home/pvetere/.config/polybar/config.ini &
-bspc config -m HDMI-1 top_padding 25
-bspc config -m HDMI-1 bottom_padding 0
+if ~/bin/is-workmon ; then
+    AUXMON=DP-3-1
+    AUXBAR=auxwork
+else
+    AUXBAR=auxwork
+    AUXMON=HDMI-1
+fi
+
+polybar -q $AUXBAR -c /home/pvetere/.config/polybar/config.ini &
+bspc config -m $AUXMON top_padding 25
+bspc config -m $AUXMON bottom_padding 0
 
